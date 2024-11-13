1e4cd2bb92ed76e078f52dcdb7acf718b3b1760e
Driann's <114148680+Driannm@users.noreply.github.com>
2024-11-13 16:09:37 +0700

    add: login and register

---
+++ resources/views/layouts/app_auth.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+    <title>{{ env('APP_NAME') }}</title>
+    <link href="../assets/css/style.css" rel="stylesheet">
+</head>
+
+<body class="font-sans" x-data="{ page: 'signin', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }" x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
+$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+    :class="{ 'dark text-bodydark bg-boxdark-2': darkMode === true }">
+    @yield('content')
+
+    <script src="../assets/js/index.js"></script>
+</body>
+
+</html>
